e317ec013e5a577b4b896197589997454fe2488c
Stepan Fedosov <zollidan@yandex.ru>
2024-11-18 14:06:20 +0300

    first commit

---
+++ src/main/java/com/lefort/olympic/OlympicApplication.java
@@ -0,0 +1,13 @@
+package com.lefort.olympic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlympicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlympicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lefort/olympic/OlympicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lefort.olympic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

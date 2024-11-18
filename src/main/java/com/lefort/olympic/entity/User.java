7af0bdb3c6092693de3bef81b85bda0b23c4fb94
Stepan <zollidan@yandex.ru>
2024-11-19 00:13:42 +0300

    create: user model

---
+++ src/main/java/com/lefort/olympic/entity/User.java
@@ -0,0 +1,20 @@
+package com.lefort.olympic.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    // Getters and Setters
+}
